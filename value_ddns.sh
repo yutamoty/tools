cba30794932acf85712ebb66dd33f4168d08248d
yuta mochizuki <yutamoty@gmail.com>
2015-03-21 12:17:46 +0900

    update value_ddns.sh

--- value_ddns.sh
+++ value_ddns.sh
@@ -2,20 +2,26 @@
 
 DOMAIN='example.com'
 PASSWD='hogehoge'
+<<<<<<< HEAD
 HOST='*'
+=======
+HOST='ddns'
+>>>>>>> b762667... Update value_ddns.sh
 
-curl -s http://dyn.value-domain.com/cgi-bin/dyn.fcg?ip -o current_ip.txt -w '%{http_code}' > status.txt
+DIR=$(dirname $0)
 
-status=$(cat status.txt)
+curl -s http://dyn.value-domain.com/cgi-bin/dyn.fcg?ip -o "${DIR}/current_ip.txt" -w '%{http_code}' > "${DIR}/status.txt"
+
+status=$(cat "${DIR}/status.txt")
 
 echo ${status}
 if [ '200' != "${status}" ] ; then
-    echo "$(date +"%Y/%m/%d-%H:%M") http status error statuscode:${status}" >> ng_log.txt
+    echo "$(date +"%Y/%m/%d-%H:%M") http status error statuscode:${status}" >> "${DIR}/ng_log.txt"
     exit 1
 fi
 
-prev=$(cat prev_ip.txt)
-current=$(cat current_ip.txt)
+prev=$(cat "${DIR}/prev_ip.txt")
+current=$(cat "${DIR}/current_ip.txt")
 
 echo ${prev}
 echo ${current}
@@ -25,7 +31,7 @@ if [ "${prev}" = "${current}" ] ; then
     exit 0
 fi
 
-cat current_ip.txt > prev_ip.txt
+cat "${DIR}/current_ip.txt" > "${DIR}/prev_ip.txt"
 
 wget -O - "http://dyn.value-domain.com/cgi-bin/dyn.fcg?d=${DOMAIN}&p=${PASSWD}&h=${HOST}&i=${current}"
 
